3b3c100111c464b43f933b9373066959fdf08f43
Erich Sickert <esickert2@gmail.com>
2020-12-16 14:13:36 -0800

    plus

--- src/com/esickert2/MenloPark.java
+++ src/com/esickert2/MenloPark.java
@@ -1,12 +1,12 @@
 package com.esickert2;
 
-import org.junit.*;
+import org.junit.Test;
 
 public class MenloPark {
 
     @Test
     public void testing() {
-        System.out.println("This is testing repository creation");//
+        System.out.println("This is testing repository creation");
     }
 
 }
